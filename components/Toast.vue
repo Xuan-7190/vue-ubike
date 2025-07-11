0efd9131d47cee8d5fdc3a7ae888ab08cdf5c385
Xuan-7190 <59555748+Xuan-7190@users.noreply.github.com>
2025-07-11 16:31:14 +0800

    Create Toast.vue

---
+++ components/Toast.vue
@@ -0,0 +1,15 @@
+<template>
+  <Teleport to="body">
+    <div v-if="visible" class="toast">
+      <div :class="['alert', typeClass]">
+        <span>{{ message }}</span>
+      </div>
+    </div>
+  </Teleport>
+</template>
+
+<script setup lang="ts">
+import { useToastState } from '../composables/useToast'
+
+const { visible, message, typeClass } = useToastState()
+</script>
